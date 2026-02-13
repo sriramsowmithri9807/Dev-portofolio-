bab8c1e5f64401f568c94741b9b55e3fc0f73107
Sriram Sowmithri <sowmithrisriram7@gmail.com>
2026-02-13 13:37:13 +0000

    fix: update mobile view photo to new profile image

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -31,7 +31,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           </div>
           {/* Mobile photo - shows only on mobile when 3D character is hidden */}
           <div className="mobile-photo">
-            <img src="/images/mypicnbg.png" alt="Sriram Sowmithri" />
+            <img src="/images/sriram.jpg" alt="Sriram Sowmithri" />
           </div>
         </div>
         {children}
